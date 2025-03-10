47a31b8f790991bf4a81f886887868902e9079a1
Amgooodd <amgdtalal3@gmail.com>
2025-03-11 01:16:24 +0200

    Update welcome_screen.dart

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class WelcomeScreen extends StatelessWidget {
   final String studentId;
 
-  WelcomeScreen({required this.studentId});
+  const WelcomeScreen({super.key, required this.studentId});
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +19,19 @@ class WelcomeScreen extends StatelessWidget {
             .get(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-           
             return Center(child: CircularProgressIndicator());
           }
 
           if (snapshot.hasError) {
-           
             return Center(child: Text("Error: ${snapshot.error}"));
           }
 
           if (!snapshot.hasData || !snapshot.data!.exists) {
-            
             return Center(child: Text("Student not found!"));
           }
 
           final studentName = snapshot.data!['name'] as String?;
 
-         
           return Center(
             child: Text(
               "Welcome, $studentName!",
